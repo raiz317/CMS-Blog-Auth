0e802d1d4a3d8516f52264b9d36a9e06798330e8
RR238 <raizzzptr@gmail.com>
2026-04-20 21:23:44 +0700

    Update host for cloud deployment

--- backend/app.js
+++ backend/app.js
@@ -92,4 +92,6 @@ app.delete('/posts/:id', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => console.log(`Backend jalan di port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server jalan di port ${PORT}`);
+});
